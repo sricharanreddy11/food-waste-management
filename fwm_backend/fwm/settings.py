01f3701b63c28455a42f518bd3c0b78997f98c2e
sricharanreddy11 <sricharanteegala@gmail.com>
2024-07-22 23:37:10 +0530

    deploy to wsgi

--- fwm_backend/fwm/settings.py
+++ fwm_backend/fwm/settings.py
@@ -29,9 +29,9 @@ STATIC_DIR = BASE_DIR.joinpath("static")
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['share-and-sustain.onrender.com']
 
 # Application definition
 
